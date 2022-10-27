927783b82025a074eb3965bc725cd24e3df92452
Serhat Biricik <biricikserhat47@gmail.com>
2022-10-27 13:36:11 +0300

    first commit

---
+++ src/test/java/com/biricik/hrms/HrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biricik.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
